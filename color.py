6754c13c1a1694aee3e18c061bfec136326deead
Alexander Mironochev <massaraksh@yandex-team.ru>
2015-05-27 01:16:05 +0300

    Добавил класс Highloghter для подсветки строк

--- color.py
+++ color.py
@@ -1,4 +1,6 @@
 from enum import Enum
+import re
+
 
 class Color(Enum):
     black = 30
@@ -15,11 +17,53 @@ class Style(Enum):
     bold = 1
     underline = 4
 
+
 def start(c: Color, s: Style = Style.normal):
     return '\033[{style};{color}m'.format(style=s.value, color=c.value)
+
 
 def end():
     return '\033[m'
 
+
 def colored(str, color: Color, style: Style = Style.normal):
     return start(color, style) + str + end()
+
+
+class ColorRule:
+    def __init__(self, regex, color: Color = Color.white, style: Style = Style.normal):
+        self.regex = re.compile(regex)
+        self.color = color
+        self.style = style
+
+    def apply(self, str):
+        def transform(s, ind, c: Color, st: Style):
+            return s[:ind[0]] + start(c, st) + s[ind[0]:ind[1]]  + end() + s[ind[1]:]
+
+        for s in self.regex.finditer(str):
+            str = transform(str, s.span(), self.color, self.style)
+            break
+        return str
+
+
+class ReplaceRule:
+    def __init__(self, regex, replace, color: Color = None, style: Style = Style.normal):
+        self.regex = regex
+        self.replace = colored(replace, color, style) if color is not None else replace
+
+    def apply(self, str):
+        return re.sub(self.regex, self.replace, str)
+
+
+class Highlighter:
+    def __init__(self, *rules):
+        self.rules = rules
+
+    def highlight(self, string):
+        for rule in self.rules:
+            string = rule.apply(string)
+        return string
+
+
+CR = ColorRule
+RR = ReplaceRule
