a74f908bf36a170334f98b5509fad9b12884c53a
Alexander Mironochev <massaraksh@yandex-team.ru>
2015-06-01 03:45:32 +0300

    Добавил класс конфигурации

---
+++ config.py
@@ -0,0 +1,21 @@
+import json
+from platform.settings import Settings
+
+class Config:
+    def __init__(self, settings = Settings(), map = None):
+        if map is None:
+            with open(settings.CONFIG_FILE, 'r') as f:
+                self.params = json.load(f)
+        else:
+            self.params = map
+
+    def serialize(self):
+        with open(Settings().CONFIG_FILE, 'w') as f:
+            json.dump(self.params, f, indent=4, sort_keys=True)
+
+    @property
+    def commandparams(self, path) -> {}:
+        for param, value in self.params['commandparams'].items():
+            if path.find(param) != -1:
+                return value
+        return {}
