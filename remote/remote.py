2202b8158bf1d1cbb415358d05968466e20f1bfc
massaraksh <massaraksh@yandex-team.ru>
2015-07-29 23:37:52 +0300

    Добавил модуль remote и имплементацию ssh

---
+++ remote/remote.py
@@ -0,0 +1,35 @@
+from subprocess import STDOUT
+
+from platform.remote.ssh import ssh
+
+
+class remote(object):
+    def __init__(self, host, impl = ssh()):
+        self._host = host
+        self._args = []
+        self._stderr = None
+        self._path = '.'
+        self._impl = impl
+
+    def cmd(self, s):
+        self._args = s
+        return self
+
+    def withstderr(self):
+        self._stderr = STDOUT
+        return self
+
+    def path(self, p):
+        self._path = p
+        return self
+
+    def call(self):
+        return self._impl.cmd(self._stderr, self._host, self._path, self._args).communicate()[0].decode('utf-8')
+
+    def exec(self):
+        p = self._impl.cmd(self._stderr, self._host, self._path, self._args)
+        while True:
+            line = p.stdout.readline().decode('utf-8')
+            if line == '':
+                break
+            yield line
